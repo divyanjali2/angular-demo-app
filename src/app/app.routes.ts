8094297ec383fa3ad72c413e68dae1a63ef26979
divyanjali2 <navodyadivyanjali2@gmail.com>
2025-03-16 10:14:22 +0530

    admin added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+    {path:'', component:AppComponent},
     {path: 'admin', component: AdminComponent}
 ];
